5e4ccabc69f496cba308957e648aec2c5edc453d
riagalv <35164865@uaz.edu.mx>
2026-03-24 22:27:44 -0600

    Listo historial

--- lib/controllers/historial_controller.dart
+++ lib/controllers/historial_controller.dart
@@ -1,4 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/alerta_model.dart';
+
+class HistorialController {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Stream<List<AlertaModel>> obtenerAlertas() {
+    return _db
+        .collection('alertas')
+        .orderBy('fecha', descending: true) // Más recientes primero
+        .snapshots()
+        .map((snapshot) {
+          return snapshot.docs.map((doc) {
+            return AlertaModel.fromFirestore(doc);
+          }).toList();
+        });
+  }
+}
+/*import '../models/alerta_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HistorialController {
@@ -12,3 +30,4 @@ class HistorialController {
     });
   }
 }
+*/
